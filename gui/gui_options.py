21b3b17fe2f5faa623414e1cd35c499e756c7c58
AlexanderRitter02 <42546705+AlexanderRitter02@users.noreply.github.com>
2022-06-23 17:48:50 +0200

    Add initial settings functionality

--- gui/gui_options.py
+++ gui/gui_options.py
@@ -1,48 +1,53 @@
-# author: Alexander Ritter
-# created on: 08/06/2022
-# edited by:
-
-# description:
 # GUI element: A seperate window for settings/options
-
 import tkinter as tk
-from tkinter import Frame, Toplevel, Label, Button
+from tkinter import Frame, Toplevel, Label, Button, Entry
 from PIL import ImageTk, Image
-
 class SettingsWindow(Toplevel):
         def __init__(self, master):
             Toplevel.__init__(self)
             self.master = master
+            self.control = control
             self.title("Settings")
             
             self.focus_set()
             self.grab_set()
             
-            body = Frame(self)
-            self.initial_focus = self.body(body)
-            body.grid(row=0, column=0, padx=5, pady=5)
+            self.content = SettingsContent(self, control)
+            self.initial_focus = self.content
+            self.content.grid(row=0, column=0, padx=5, pady=5)
             
-            self.bind("<Escape>", self.cancel)
+            self.bind("<Escape>", self.content.cancel)
             self.wait_window(self)
-        
-        
-        def body(self, master):
-            # create dialog body.  return widget that should have
-            # initial focus.  this method should be overridden
-            lbl_settings = Label(master=master, text="Settings")
-            btn_ok = Button(master=master, text="Ok", command=self.accept)
-            btn_cancel = Button(master=master, text="Cancel", command=self.cancel)
             
-            lbl_settings.grid(row=0, column=0, columnspan=2)
-            btn_cancel.grid(row=1, column=0)
-            btn_ok.grid(row=1, column=1)
-            pass
+class SettingsContent(Frame):
+    def __init__(self, master, control):
+        Frame.__init__(self, master)
+        self.master = master
         
-        def accept(self, event=None):
-            pass
+        lbl_aspect = Label(master=self, text="Aspect ratio")
+        self.ent_width  = Entry(master=self)
+        self.ent_height = Entry(master=self)
         
-        def cancel(self, event=None):
-            self.master.focus_set()
-            self.destroy()
-            pass
+        lbl_settings = Label(master=self, text="Settings")
+        btn_ok = Button(master=self, text="Ok", command=self.accept)
+        btn_cancel = Button(master=self, text="Cancel", command=self.cancel)
             
+        lbl_settings.grid(row=0, column=0, columnspan=2)
+        lbl_aspect.grid(row=1, column=0)
+        self.ent_width.grid(row=1, column=1)
+        self.ent_height.grid(row=1, column=2)
+        btn_cancel.grid(row=2, column=0)
+        btn_ok.grid(row=2, column=1)
+        
+    def accept(self):
+        w = int(self.ent_width.get())
+        h = int(self.ent_height.get())
+        self.close_window()
+        
+    def cancel(self, event=None):
+        print("Closing window")
+        self.close_window()
+        
+    def close_window(self):
+        self.master.focus_set()
+        self.master.destroy()
---
+++ gui/settings.py
@@ -0,0 +1,54 @@
+# author: Alexander Ritter
+# created on: 16/06/2022
+# edited by:
+
+# description:
+# Contains the structure of the Settings class and functions for loading/saving it
+
+from dataclasses import dataclass
+import typing as t
+import yaml
+
+@dataclass
+class AspectRatio:
+    width: int
+    height: int
+    
+    @classmethod
+    def from_dict(cls: t.Type["AspectRatio"], dic: dict):
+        return cls(width=dic["width"], height=dic["height"])
+
+@dataclass
+class Settings():
+
+    auto_updatecheck: bool
+    aspect: AspectRatio
+    
+    @classmethod
+    def from_dict(cls: t.Type["Settings"], dic: dict):
+        return cls(
+            auto_updatecheck=dic["auto_updatecheck"],
+            aspect = AspectRatio.from_dict(dic["aspect"])
+        )
+    
+    @staticmethod
+    def set_aspect_ratio(self, width: int, height: int) -> None:
+        self.aspect.width  = width
+        self.aspect.height = height
+        # TODO Change the aspect ratio of the camera in here as well
+
+# Parses and returns a Settings object
+# May return NoneType, please check outside
+def load_settings() -> Settings:
+    with open("settings.yaml", "r") as f:
+        config = yaml.safe_load(f)    
+        print(config)
+    try:
+        settings = Settings.from_dict(config)
+        return settings
+    except:
+        print("Parsing error during config loading, please regenerate it")
+       
+def save_settings(settings: Settings) -> None:
+    print("Saving configuration")
+    pass
