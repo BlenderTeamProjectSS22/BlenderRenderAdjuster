3c0b9453fb1f7cec7b0765b87096fa75ddbc9658
BuchiYu55 <103631941+BuchiYu55@users.noreply.github.com>
2022-06-29 15:01:24 +0200

    connect the materials to Brancn:Texture

--- Texture.py
+++ Texture.py
@@ -1,21 +1,21 @@
 import bpy
 
 ##make a new materials called 'Tex', and we use this materials for the Object with suitble texture.
-def object_to_material(obj: bpy.types.Object):
-  mat = bpy.data.materials.new('Tex')
-  mat.use_nodes=True
+#def object_to_material(obj: bpy.types.Object):
+ # mat = bpy.data.materials.new('Tex')
+  #mat.use_nodes=True
 
   ##select the object and append with material mat
-  obj.data.materials.append(mat)
+  #obj.data.materials.append(mat)
 
 #import the Texture image
-def load_texture(texture_path: str):
-  texImage = mat.node_tree.nodes.new('ShaderNodeTexImage')
+def load_texture(texture_path: str, material: bpy.types.Material):
+  texImage = material.node_tree.nodes.new('ShaderNodeTexImage')
   texImage.image = bpy.data.image.load(texture_path)
   
   #disp is path of Base color
-  disp=bpy.data.materials['Tex'].node_tree.nodes["Principled BSDF"].inputs['Base Color']
+  disp=material.node_tree.nodes["Principled BSDF"].inputs['Base Color']
       
   #connect the node between 'Base Color' of bsdf and 'Color' of texImage
-  mat.node_tree.links.new(disp,texImage.outputs[0])
+  material.node_tree.links.new(disp,texImage.outputs[0])
 
