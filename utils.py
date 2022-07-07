a06929fac817af7cb3d2ce2f31e3fb7c1dfc29bb
j-w-moebius <johann.moebius@gmx.net>
2022-07-07 17:57:11 +0200

    fix bug with vertical panning

--- utils.py
+++ utils.py
@@ -67,7 +67,7 @@ class OrbitCam:
 
     # pan camera along global Z axis, changing its position by distance
     def pan_vertical(self, distance: float) -> None:
-        bpy.context.object.location[2] += distance
+        self.controller.location[2] += distance
 
     # pan camera along local Y axis, chaning its position by distance
     # relies on bpy.ops.tranform because of the use of local axis
