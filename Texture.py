215ee8f79b60634dfe884b86c374f2fca0ed90ea
BuchiYu55 <1318024391@qq.com>
2022-07-13 18:32:07 +0200

    fix the problem: when people render with more then 1 Material, it bekomm more then 1 Image_>Texture_Node

--- Texture.py
+++ Texture.py
@@ -2,7 +2,11 @@ import bpy
 
 #import the Texture image
 def load_texture(texture_path: str, material: bpy.types.Material):
-  texImage = material.node_tree.nodes.new('ShaderNodeTexImage')
+  if(material.node_tree.nodes.find('ShaderNodeTexImage') == -1):
+    texImage = material.node_tree.nodes.new('ShaderNodeTexImage')
+  else:
+    texImage = material.node_tree.nodes['Image Texture']
+    
   texImage.image = bpy.data.images.load(bpy.path.relpath(texture_path))
   
   #disp is path of Base color
@@ -12,5 +16,9 @@ def load_texture(texture_path: str, material: bpy.types.Material):
   material.node_tree.links.new(disp,texImage.outputs[0])
 
 def delete_texture(material: bpy.types.Material):
+  if(material.node_tree.nodes.find('ShaderNodeTexImage') != -1):
+    node_to_delete =  material.node_tree.nodes['Image Texture']
+    material.node_tree.nodes.remove[node_to_delete]
+    
   disp=material.node_tree.nodes["Principled BSDF"].inputs['Base Color']
   material.node_tree.links.remove(disp.links[0])
