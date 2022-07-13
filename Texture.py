15afafb92eb789f8a48e2dc27c3aea6dede72662
AlexanderRitter02 <42546705+AlexanderRitter02@users.noreply.github.com>
2022-07-13 19:45:32 +0200

    Fix creation of unlimited new nodes

--- Texture.py
+++ Texture.py
@@ -2,20 +2,19 @@ import bpy
 
 #import the Texture image
 def load_texture(texture_path: str, material: bpy.types.Material):
-  while(material.node_tree.nodes.find('ShaderNodeTexImage') != -1):
-    node_to_delete =  material.node_tree.nodes['Image Texture']
-    material.node_tree.nodes.remove[node_to_delete]
-
-  texImage = material.node_tree.nodes.new('ShaderNodeTexImage')
+  if material.node_tree.nodes.get("Image Texture") is None:
+    texImage = material.node_tree.nodes.new(type = "ShaderNodeTexImage")
+  else:
+    texImage = material.node_tree.nodes.get("Image Texture")
   texImage.image = bpy.data.images.load(bpy.path.relpath(texture_path))
   
   #disp is path of Base color
-  disp=material.node_tree.nodes["Principled BSDF"].inputs['Base Color']
+  disp=material.node_tree.nodes["Principled BSDF"].inputs["Base Color"]
       
   #connect the node between 'Base Color' of bsdf and 'Color' of texImage
   material.node_tree.links.new(disp,texImage.outputs[0])
   
 
 def delete_texture(material: bpy.types.Material):
-  disp=material.node_tree.nodes["Principled BSDF"].inputs['Base Color']
+  disp=material.node_tree.nodes["Principled BSDF"].inputs["Base Color"]
   material.node_tree.links.remove(disp.links[0])
