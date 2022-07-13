31bc1f40b6ac500cfd77d24d14239055260d9f23
BuchiYu55 <1318024391@qq.com>
2022-07-13 19:22:41 +0200

    remove didnt work

--- Texture.py
+++ Texture.py
@@ -2,11 +2,11 @@ import bpy
 
 #import the Texture image
 def load_texture(texture_path: str, material: bpy.types.Material):
-  if(material.node_tree.nodes.find('ShaderNodeTexImage') == -1):
-    texImage = material.node_tree.nodes.new('ShaderNodeTexImage')
-  else:
-    texImage = material.node_tree.nodes['Image Texture']
-    
+  while(material.node_tree.nodes.find('ShaderNodeTexImage') != -1):
+    node_to_delete =  material.node_tree.nodes['Image Texture']
+    material.node_tree.nodes.remove[node_to_delete]
+
+  texImage = material.node_tree.nodes.new('ShaderNodeTexImage')
   texImage.image = bpy.data.images.load(bpy.path.relpath(texture_path))
   
   #disp is path of Base color
@@ -14,11 +14,8 @@ def load_texture(texture_path: str, material: bpy.types.Material):
       
   #connect the node between 'Base Color' of bsdf and 'Color' of texImage
   material.node_tree.links.new(disp,texImage.outputs[0])
+  
 
 def delete_texture(material: bpy.types.Material):
-  if(material.node_tree.nodes.find('ShaderNodeTexImage') != -1):
-    node_to_delete =  material.node_tree.nodes['Image Texture']
-    material.node_tree.nodes.remove[node_to_delete]
-    
   disp=material.node_tree.nodes["Principled BSDF"].inputs['Base Color']
   material.node_tree.links.remove(disp.links[0])
