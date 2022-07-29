f03e16bd5e0e187003e7e798114e24e59a6c13fc
AlexanderRitter02 <42546705+AlexanderRitter02@users.noreply.github.com>
2022-07-30 00:43:15 +0200

    Update version number to 1.0.0

--- gui/properties.py
+++ gui/properties.py
@@ -1,7 +1,7 @@
 # This modul contains "constants"
 UPDATE_URL = "https://raw.githubusercontent.com/AlexanderRitter02/update_checks/master/blender_render.txt"
 
-VERSION_MAJOR = 0
+VERSION_MAJOR = 1
 VERSION_MINOR = 0
 VERSION_PATCH = 0
 
